b4c3f174e9f7fc60d7f0e512343d3d15d2c4e654
Enver Fransazov <161112453+FransazovEnver@users.noreply.github.com>
2026-01-06 14:46:09 +0200

    commit

--- 06.ForAndWhileLoops/02.FirstNNumbersSum.cs
+++ 06.ForAndWhileLoops/02.FirstNNumbersSum.cs
@@ -8,14 +8,6 @@ int sum = 0;
 //Logic
 for (int num = startNum; num <= endNum; num++)
 {
-    if (num != endNum)
-    {
-        Console.Write(num + "+");
-    }
-    else
-    {
-        Console.Write(num + "=");
-    }
     sum += num;
 }
 Console.Write(sum);
