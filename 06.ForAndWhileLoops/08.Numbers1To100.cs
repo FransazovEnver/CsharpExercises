bf6a9d564ba79c7eb858ccc6bb9e73f5d63b8596
Enver Fransazov <161112453+FransazovEnver@users.noreply.github.com>
2026-01-06 21:02:58 +0200

    commit

---
+++ 06.ForAndWhileLoops/08.Numbers1To100.cs
@@ -0,0 +1,17 @@
+﻿//Input
+//While Loop
+/*
+int number = 1;
+while (number <= 100)
+{
+    Console.WriteLine(number);
+    number++;
+}
+
+*/
+
+//For Loop
+for (int i = 1; i <= 100; i++)
+{
+    Console.WriteLine(i);
+}
---
+++ 06.ForAndWhileLoops/09.DecreasingNumbers.cs
@@ -0,0 +1,11 @@
+﻿//Input
+int firstNumber = int.Parse(Console.ReadLine());
+
+
+//Logic
+int secondNumber = firstNumber;
+while (secondNumber >= 1)
+{
+    Console.WriteLine(secondNumber);
+    secondNumber--;
+}
