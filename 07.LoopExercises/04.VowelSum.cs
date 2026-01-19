b54b1b0a75557b1ea9d715e31a3499369986de67
Enver Fransazov <161112453+FransazovEnver@users.noreply.github.com>
2026-01-19 20:23:01 +0200

    commit

---
+++ 07.LoopExercises/04.VowelSum.cs
@@ -0,0 +1,32 @@
+﻿//Input
+int vowel = int.Parse(Console.ReadLine());
+
+int sum = 0;
+//Logic
+for (int i = 0; i < vowel; i++)
+{
+    char letter = char.Parse(Console.ReadLine());
+
+    switch (letter)
+    {
+        case 'a':
+            sum += 1;
+            break;
+        case 'e':
+            sum += 2;
+            break;
+        case 'i':
+            sum += 3;
+            break;
+        case 'o':
+            sum += 4;
+            break;
+        case 'u':
+            sum += 5;
+            break;
+    }
+}
+Console.WriteLine(sum);
+
+
+
---
+++ 07.LoopExercises/05.DivisionTo2,3and4.cs
@@ -0,0 +1,32 @@
+﻿//Input
+
+int num = int.Parse(Console.ReadLine());
+
+//Logic
+int countDiBy2 = 0;
+int countDiBy3 = 0;
+int countDiBy4 = 0;
+
+for (int i = 0; i < num; i++)
+{
+    int curNum = int.Parse(Console.ReadLine());
+    if (curNum % 2 == 0)
+    {
+        countDiBy2++;
+    }
+    if (curNum % 3 == 0)
+    {
+        countDiBy3++;
+    }
+    if (curNum % 4 == 0)
+    {
+        countDiBy4++;
+    }
+}
+double perDivBy2 = (double)countDiBy2 / num * 100;
+double perDivBy3 = (double)countDiBy3 / num * 100;
+double perDivBy4 = (double)countDiBy4 / num * 100;
+
+Console.WriteLine($"{perDivBy2:F2}%");
+Console.WriteLine($"{perDivBy3:F2}%");
+Console.WriteLine($"{perDivBy4:F2}%");
