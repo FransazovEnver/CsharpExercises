019d628403f805ca2ebe506cc35189fb5b128ecd
Enver Fransazov <161112453+FransazovEnver@users.noreply.github.com>
2026-01-22 18:45:17 +0200

    write a program to check if given number is special

---
+++ 07.LoopExercises/06.SpecialNumber.cs
@@ -0,0 +1,25 @@
+﻿//Input 
+int num = int.Parse(Console.ReadLine());
+int temp = num;
+
+bool isSpecial = false;
+
+while (temp > 0)
+{
+    int lastNumber = temp % 10;
+
+    temp = temp / 10;
+
+    if (num % lastNumber != 0)
+
+        break;
+}
+
+if (isSpecial)
+{
+    Console.WriteLine($"{num} is special");
+}
+else
+{
+    Console.WriteLine($"{num} is not special");
+}
